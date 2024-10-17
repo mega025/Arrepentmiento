3e3cd4bad9d330023e348ab3930d79032f529412
mega025 <xinjiepan123@gmail.com>
2024-10-17 11:31:51 +0200

    Main.java definitivo

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main (String[] args){
-		for (int i = 0 ; i < args.length; i++){
+		for (int i = 0 ; i < args.length; i++){ 
 			System.out.println("Argumentos de Santi " + i + ": " + args[i]);
 		} 
 	}
