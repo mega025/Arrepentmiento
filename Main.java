0506673d2fdd2d5f44ca8bb6bc7af332d23c80cf
mega025 <xinjiepan123@gmail.com>
2024-10-17 11:43:37 +0200

    Main.java terminado para enviar

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main{
 	public static void main (String[] args){
 		for (int i = 0 ; i < args.length; i++){ 
+		try{
 			System.out.println("Argumentos de Santi " + i + ": " + args[i]);
-		} 
+		} catch (ArrayIndexOutOfBoundsException e){
+			System.out.println("Error " + e.getMessage());
 	}
+	}
+}
 }
